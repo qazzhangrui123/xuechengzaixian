2e1a399c8980633c822d2ee51737740a4f7ed4ee
刘老板 <5745480+liulaoban998@user.noreply.gitee.com>
2023-05-12 09:48:56 +0800

    接口测试

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
@@ -1,5 +1,6 @@
 package com.xuecheng.base.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
 
@@ -10,8 +11,10 @@ import lombok.ToString;
 @ToString
 public class PageParams {
     //当前页码
+    @ApiModelProperty("页码")
     private Long pageNo =1L;
     //每页显示记录数
+    @ApiModelProperty("记录数")
     private Long pageSize =30L;
 
     public PageParams() {
