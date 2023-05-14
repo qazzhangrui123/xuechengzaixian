6f54269248ff4b924a4d75be144f152f4acb8d4c
刘老板 <5745480+liulaoban998@user.noreply.gitee.com>
2023-05-14 14:56:45 +0800

    部署系统管理服务和接口测试

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -0,0 +1,18 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+
+
+public interface CourseBaseInfoService {
+    /**
+     * 课程分页查询
+     * @param pageParams
+     * @param courseParamsDto
+     * @return
+     */
+    public PageResult<CourseBase>  queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto courseParamsDto);
+
+}
---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/CourseBaseInfoServiceTests.java
@@ -0,0 +1,39 @@
+package com.xuecheng.content;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class CourseBaseInfoServiceTests {
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+
+    @Test
+    public void testCourseBaseMapper(){
+
+        QueryCourseParamsDto queryCourseParamsDto = new QueryCourseParamsDto();
+        queryCourseParamsDto.setCourseName("java");  //课程名称查询条件
+        queryCourseParamsDto.setAuditStatus("202004");
+
+        //分页参数对象
+        PageParams pageParams = new PageParams();
+        pageParams.setPageNo(2L);
+        pageParams.setPageSize(2L);
+
+        PageResult<CourseBase> courseBasePageResult = courseBaseInfoService.queryCourseBaseList(pageParams,queryCourseParamsDto);
+        System.out.println(courseBasePageResult);
+    }
+}
